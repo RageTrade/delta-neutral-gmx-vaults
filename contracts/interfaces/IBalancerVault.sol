1f198e086351b7a62158e85725abe574fd17ab28
protodev-rage <protodev@rage.trade>
2022-09-05 15:40:20 +0530

    feat: add balancer flashloan

---
+++ contracts/interfaces/IBalancerVault.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IBalancerVault {
+    /**
+     * @dev Performs a 'flash loan', sending tokens to `recipient`, executing the `receiveFlashLoan` hook on it,
+     * and then reverting unless the tokens plus a proportional protocol fee have been returned.
+     *
+     * The `tokens` and `amounts` arrays must have the same length, and each entry in these indicates the loan amount
+     * for each token contract. `tokens` must be sorted in ascending order.
+     *
+     * The 'userData' field is ignored by the Vault, and forwarded as-is to `recipient` as part of the
+     * `receiveFlashLoan` call.
+     *
+     * Emits `FlashLoan` events.
+     */
+    function flashLoan(
+        address recipient,
+        address[] memory tokens,
+        uint256[] memory amounts,
+        bytes memory userData
+    ) external;
+
+    /**
+     * @dev Returns `user`'s Internal Balance for a set of tokens.
+     */
+    function getInternalBalance(address user, address[] memory tokens) external view returns (uint256[] memory);
+}
