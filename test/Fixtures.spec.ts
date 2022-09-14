229f2b4802b429f0f6e916bef248780e0f0bc61f
protodev-rage <protodev@rage.trade>
2022-09-14 16:59:52 +0530

    test: initial spec files

--- test/Fixtures.spec.ts
+++
@@ -1,7 +0,0 @@
-import { dnGmxVaultFixture } from './fixtures/dn-gmx-vault';
-
-describe('check fixtures', () => {
-  it('works', async () => {
-    const contracts = await dnGmxVaultFixture();
-  });
-});
