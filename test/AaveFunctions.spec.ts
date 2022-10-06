81dc6c43f3f5ea4032445dc6e356af34a467d4b4
protodev-rage <protodev@rage.trade>
2022-10-06 11:24:27 +0530

    chore: add expects to aave interactions test cases

--- test/AaveFunctions.spec.ts
+++ test/AaveFunctions.spec.ts
@@ -1,5 +1,4 @@
 import { expect } from 'chai';
-import hre, { ethers } from 'hardhat';
 import { generateErc20Balance } from './utils/generator';
 import { parseEther, parseUnits } from 'ethers/lib/utils';
 import { dnGmxJuniorVaultFixture } from './fixtures/dn-gmx-junior-vault';
@@ -7,33 +6,73 @@ import { dnGmxJuniorVaultFixture } from './fixtures/dn-gmx-junior-vault';
 describe('Aave interactions & functions', () => {
   it('executeSupply', async () => {
     const amount = parseUnits('100', 6);
-    const { dnGmxJuniorVault, usdc } = await dnGmxJuniorVaultFixture();
+    const { dnGmxJuniorVault, usdc, aUSDC } = await dnGmxJuniorVaultFixture();
 
     await generateErc20Balance(usdc, amount, dnGmxJuniorVault.address);
 
+    expect(await aUSDC.balanceOf(dnGmxJuniorVault.address)).to.eq(0);
+    expect(await usdc.balanceOf(dnGmxJuniorVault.address)).to.eq(amount);
+
     await dnGmxJuniorVault.executeSupply(usdc.address, amount);
+
+    expect(await usdc.balanceOf(dnGmxJuniorVault.address)).to.eq(0);
+    expect(await aUSDC.balanceOf(dnGmxJuniorVault.address)).to.eq(amount);
   });
 
   it('executeWithdraw', async () => {
     const amount = parseUnits('100', 6);
-    const { dnGmxJuniorVault, usdc } = await dnGmxJuniorVaultFixture();
+    const { dnGmxJuniorVault, usdc, aUSDC } = await dnGmxJuniorVaultFixture();
     await generateErc20Balance(usdc, amount, dnGmxJuniorVault.address);
 
+    expect(await aUSDC.balanceOf(dnGmxJuniorVault.address)).to.eq(0);
+    expect(await usdc.balanceOf(dnGmxJuniorVault.address)).to.eq(amount);
+
     await dnGmxJuniorVault.executeSupply(usdc.address, amount);
+
+    expect(await usdc.balanceOf(dnGmxJuniorVault.address)).to.eq(0);
+    expect(await aUSDC.balanceOf(dnGmxJuniorVault.address)).to.eq(amount);
+
     await dnGmxJuniorVault.executeWithdraw(usdc.address, amount);
+
+    expect(await aUSDC.balanceOf(dnGmxJuniorVault.address)).to.eq(0);
+    expect(await usdc.balanceOf(dnGmxJuniorVault.address)).to.eq(amount);
   });
 
-  it('executeBorrow & executeRepay', async () => {
+  it.only('executeBorrow & executeRepay', async () => {
     const amount = parseUnits('100', 6);
-    const { dnGmxJuniorVault, usdc, wbtc, weth } = await dnGmxJuniorVaultFixture();
+    const btcAmount = parseUnits('0.002', 8);
+    const ethAmount = parseEther('0.02');
+
+    const { dnGmxJuniorVault, usdc, wbtc, weth, aUSDC, vdWBTC, vdWETH } = await dnGmxJuniorVaultFixture();
     await generateErc20Balance(usdc, amount, dnGmxJuniorVault.address);
+
+    expect(await aUSDC.balanceOf(dnGmxJuniorVault.address)).to.eq(0);
+    expect(await usdc.balanceOf(dnGmxJuniorVault.address)).to.eq(amount);
 
     await dnGmxJuniorVault.executeSupply(usdc.address, amount);
 
-    await dnGmxJuniorVault.executeBorrow(wbtc.address, parseUnits('0.002', 8));
-    await dnGmxJuniorVault.executeBorrow(weth.address, parseEther('0.02'));
+    expect(await usdc.balanceOf(dnGmxJuniorVault.address)).to.eq(0);
+    expect(await aUSDC.balanceOf(dnGmxJuniorVault.address)).to.eq(amount);
+
+    const txBtc = await dnGmxJuniorVault.executeBorrow(wbtc.address, btcAmount);
+    const txEth = await dnGmxJuniorVault.executeBorrow(weth.address, ethAmount);
+
+    expect(
+      await vdWBTC.balanceOf(dnGmxJuniorVault.address, {
+        blockTag: txBtc.blockNumber,
+      }),
+    ).to.eq(btcAmount);
 
     await dnGmxJuniorVault.executeRepay(wbtc.address, parseUnits('0.002', 8));
+    expect(await vdWBTC.balanceOf(dnGmxJuniorVault.address)).to.eq(0);
+
+    expect(
+      await vdWETH.balanceOf(dnGmxJuniorVault.address, {
+        blockTag: txEth.blockNumber,
+      }),
+    ).to.eq(ethAmount);
+
     await dnGmxJuniorVault.executeRepay(weth.address, parseEther('0.02'));
+    expect(await vdWETH.balanceOf(dnGmxJuniorVault.address)).to.lte(parseUnits('1', 8));
   });
 });
