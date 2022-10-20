dc53067c7e9c174d5a6debf53ba15195bb07be02
protodev-rage <protodev@rage.trade>
2022-10-20 19:11:51 +0530

    ref: cleanup senior vault & add setter for leverage pool

--- contracts/vaults/DnGmxSeniorVault.sol
+++ contracts/vaults/DnGmxSeniorVault.sol
@@ -1,51 +1,42 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.9;
-import { FullMath } from '@uniswap/v3-core-0.8-support/contracts/libraries/FullMath.sol';
+
+import { IBorrower } from '../interfaces/IBorrower.sol';
+
 import { IPool } from '@aave/core-v3/contracts/interfaces/IPool.sol';
-import { IAToken } from '@aave/core-v3/contracts/interfaces/IAToken.sol';
-import { IPoolAddressesProvider } from '@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol';
 import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import { IERC20Metadata } from '@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol';
+import { IAToken } from '@aave/core-v3/contracts/interfaces/IAToken.sol';
+import { IPriceOracle } from '@aave/core-v3/contracts/interfaces/IPriceOracle.sol';
+import { IPoolAddressesProvider } from '@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol';
+
+import { IERC4626 } from '../interfaces/IERC4626.sol';
+import { IDnGmxSeniorVault } from '../interfaces/IDnGmxSeniorVault.sol';
+import { ERC4626Upgradeable } from '../ERC4626/ERC4626Upgradeable.sol';
 import { OwnableUpgradeable } from '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 import { PausableUpgradeable } from '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
-import { ERC4626Upgradeable } from '../ERC4626/ERC4626Upgradeable.sol';
+
 import { FeeSplitStrategy } from '../libraries/FeeSplitStrategy.sol';
-import { IDnGmxJuniorVault } from '../interfaces/IDnGmxJuniorVault.sol';
-import { ILeveragePool } from '../interfaces/ILeveragePool.sol';
-import { IBorrower } from '../interfaces/IBorrower.sol';
-import { IPriceOracle } from '@aave/core-v3/contracts/interfaces/IPriceOracle.sol';
+import { FullMath } from '@uniswap/v3-core-0.8-support/contracts/libraries/FullMath.sol';
 
-import 'hardhat/console.sol';
-
-contract DnGmxSeniorVault is ERC4626Upgradeable, OwnableUpgradeable, PausableUpgradeable {
-    using FeeSplitStrategy for FeeSplitStrategy.Info;
+contract DnGmxSeniorVault is IDnGmxSeniorVault, ERC4626Upgradeable, OwnableUpgradeable, PausableUpgradeable {
     using FullMath for uint256;
-
-    error CallerNotBorrower();
-    error UsageCapExceeded();
-    error InvalidBorrowAmount();
-    error InvalidBorrowerAddress();
-    error InvalidCapUpdate();
-    error MaxUtilizationBreached();
-    error DepositCapExceeded();
-
-    event AllowancesGranted();
-    event BorrowCapUpdated(address vault, uint256 newCap);
-    event DepositCapUpdated(uint256 _newDepositCap);
+    using FeeSplitStrategy for FeeSplitStrategy.Info;
 
     uint16 internal constant MAX_BPS = 10_000;
 
+    uint256 public depositCap;
+    uint256 public maxUtilizationBps;
+
+    IBorrower public leveragePool;
+    IBorrower public dnGmxJuniorVault;
+
+    FeeSplitStrategy.Info public feeStrategy;
+
     IPool internal pool;
     IAToken internal aUsdc;
-    IPoolAddressesProvider internal poolAddressProvider;
-    FeeSplitStrategy.Info public feeStrategy;
-    IDnGmxJuniorVault public dnGmxJuniorVault;
-    ILeveragePool public leveragePool;
     IPriceOracle internal oracle;
-
-    uint16 public maxUtilizationBps;
-    uint256 public depositCap;
+    IPoolAddressesProvider internal poolAddressProvider;
 
     mapping(address => uint256) public borrowCaps;
 
@@ -71,15 +62,7 @@ contract DnGmxSeniorVault is ERC4626Upgradeable, OwnableUpgradeable, PausableUpg
         oracle = IPriceOracle(poolAddressProvider.getPriceOracle());
 
         aUsdc.approve(address(pool), type(uint256).max);
-        IERC20Metadata(asset).approve(address(pool), type(uint256).max);
-    }
-
-    function decimals() public view override returns (uint8) {
-        return 6;
-    }
-
-    function setDnGmxJuniorVault(address _dnGmxJuniorVault) external onlyOwner {
-        dnGmxJuniorVault = IDnGmxJuniorVault(_dnGmxJuniorVault);
+        IERC20(asset).approve(address(pool), type(uint256).max);
     }
 
     function setDepositCap(uint256 _newDepositCap) external onlyOwner {
@@ -87,14 +70,22 @@ contract DnGmxSeniorVault is ERC4626Upgradeable, OwnableUpgradeable, PausableUpg
         emit DepositCapUpdated(_newDepositCap);
     }
 
-    function setMaxUtilizationBps(uint16 _maxUtilizationBps) external onlyOwner {
+    function setLeveragePool(IBorrower _leveragePool) external onlyOwner {
+        leveragePool = _leveragePool;
+    }
+
+    function setDnGmxJuniorVault(IBorrower _dnGmxJuniorVault) external onlyOwner {
+        dnGmxJuniorVault = _dnGmxJuniorVault;
+    }
+
+    function setMaxUtilizationBps(uint256 _maxUtilizationBps) external onlyOwner {
         maxUtilizationBps = _maxUtilizationBps;
     }
 
     function grantAllowances() external onlyOwner {
         address aavePool = address(pool);
 
-        IERC20Metadata(asset).approve(aavePool, type(uint256).max);
+        IERC20(asset).approve(aavePool, type(uint256).max);
         aUsdc.approve(aavePool, type(uint256).max);
 
         emit AllowancesGranted();
@@ -104,15 +95,12 @@ contract DnGmxSeniorVault is ERC4626Upgradeable, OwnableUpgradeable, PausableUpg
         if (borrowerAddress != address(dnGmxJuniorVault) && borrowerAddress != address(leveragePool))
             revert InvalidBorrowerAddress();
 
-        if (IBorrower(borrowerAddress).getUsdcBorrowed() < cap) {
-            borrowCaps[borrowerAddress] = cap;
+        if (IBorrower(borrowerAddress).getUsdcBorrowed() >= cap) revert InvalidCapUpdate();
 
-            aUsdc.approve(borrowerAddress, cap);
+        borrowCaps[borrowerAddress] = cap;
+        aUsdc.approve(borrowerAddress, cap);
 
-            emit BorrowCapUpdated(borrowerAddress, cap);
-        } else {
-            revert InvalidCapUpdate();
-        }
+        emit BorrowCapUpdated(borrowerAddress, cap);
     }
 
     function updateFeeStrategyParams(FeeSplitStrategy.Info calldata _feeStrategy) external onlyOwner {
@@ -131,27 +119,35 @@ contract DnGmxSeniorVault is ERC4626Upgradeable, OwnableUpgradeable, PausableUpg
     }
 
     function borrow(uint256 amount) external onlyBorrower {
+        if (amount == 0 || amount > availableBorrow(msg.sender)) revert InvalidBorrowAmount();
+
         dnGmxJuniorVault.harvestFees();
-        if (amount == 0 || amount > availableBorrow(msg.sender)) {
-            revert InvalidBorrowAmount();
-        } else {
-            aUsdc.transfer(msg.sender, amount);
-        }
+        aUsdc.transfer(msg.sender, amount);
     }
 
     function repay(uint256 amount) external onlyBorrower {
-        console.log('msg.sender bal', aUsdc.balanceOf(msg.sender));
-        console.log('amount_', amount);
         dnGmxJuniorVault.harvestFees();
         aUsdc.transferFrom(msg.sender, address(this), amount);
     }
 
-    function deposit(uint256 amount, address to) public virtual override whenNotPaused returns (uint256 shares) {
+    function deposit(uint256 amount, address to)
+        public
+        virtual
+        override(IERC4626, ERC4626Upgradeable)
+        whenNotPaused
+        returns (uint256 shares)
+    {
         dnGmxJuniorVault.harvestFees();
         shares = super.deposit(amount, to);
     }
 
-    function mint(uint256 shares, address to) public virtual override whenNotPaused returns (uint256 amount) {
+    function mint(uint256 shares, address to)
+        public
+        virtual
+        override(IERC4626, ERC4626Upgradeable)
+        whenNotPaused
+        returns (uint256 amount)
+    {
         dnGmxJuniorVault.harvestFees();
         amount = super.mint(shares, to);
     }
@@ -160,7 +156,7 @@ contract DnGmxSeniorVault is ERC4626Upgradeable, OwnableUpgradeable, PausableUpg
         uint256 assets,
         address receiver,
         address owner
-    ) public override whenNotPaused returns (uint256 shares) {
+    ) public override(IERC4626, ERC4626Upgradeable) whenNotPaused returns (uint256 shares) {
         dnGmxJuniorVault.harvestFees();
         shares = super.withdraw(assets, receiver, owner);
     }
@@ -169,7 +165,7 @@ contract DnGmxSeniorVault is ERC4626Upgradeable, OwnableUpgradeable, PausableUpg
         uint256 shares,
         address receiver,
         address owner
-    ) public override whenNotPaused returns (uint256 assets) {
+    ) public override(IERC4626, ERC4626Upgradeable) whenNotPaused returns (uint256 assets) {
         dnGmxJuniorVault.harvestFees();
         assets = super.redeem(shares, receiver, owner);
     }
@@ -181,9 +177,8 @@ contract DnGmxSeniorVault is ERC4626Upgradeable, OwnableUpgradeable, PausableUpg
         address
     ) internal override {
         // check if the utilization goes above limit due to this withdrawal
-        if (totalUsdcBorrowed() > ((totalAssets() - assets) * maxUtilizationBps) / MAX_BPS) {
+        if (totalUsdcBorrowed() > ((totalAssets() - assets) * maxUtilizationBps) / MAX_BPS)
             revert MaxUtilizationBreached();
-        }
         pool.withdraw(address(asset), assets, address(this));
     }
 
@@ -192,32 +187,36 @@ contract DnGmxSeniorVault is ERC4626Upgradeable, OwnableUpgradeable, PausableUpg
         uint256,
         address
     ) internal override {
-        pool.supply(address(asset), assets, address(this), 0);
-
         if (totalAssets() > depositCap) revert DepositCapExceeded();
+
+        pool.supply(address(asset), assets, address(this), 0);
     }
 
-    function totalAssets() public view override returns (uint256 amount) {
+    function totalAssets() public view override(IERC4626, ERC4626Upgradeable) returns (uint256 amount) {
         amount = aUsdc.balanceOf(address(this));
-
         amount += totalUsdcBorrowed();
     }
 
     function totalUsdcBorrowed() public view returns (uint256 usdcBorrowed) {
-        if (address(dnGmxJuniorVault) != address(0)) usdcBorrowed += dnGmxJuniorVault.getUsdcBorrowed();
         if (address(leveragePool) != address(0)) usdcBorrowed += leveragePool.getUsdcBorrowed();
+        if (address(dnGmxJuniorVault) != address(0)) usdcBorrowed += dnGmxJuniorVault.getUsdcBorrowed();
     }
 
     function getPriceX128() public view returns (uint256) {
         uint256 price = oracle.getAssetPrice(address(asset));
 
         // @dev aave returns from same source as chainlink (which is 8 decimals)
-        return price.mulDiv(1 << 128, 1e8); // usdc decimals - (chainlink decimals + asset decimals) = 6-8-6 = 8
+        // usdc decimals - (chainlink decimals + asset decimals) = 6-8-6 = 8
+        return price.mulDiv(1 << 128, 1e8);
     }
 
     function getVaultMarketValue() public view returns (uint256) {
         uint256 price = oracle.getAssetPrice(address(asset));
 
         return totalAssets().mulDiv(price, 1e8);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return 6;
     }
 }
