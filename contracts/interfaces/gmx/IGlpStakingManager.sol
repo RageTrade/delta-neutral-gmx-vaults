7db79206f8e9977bab22b121cbce805c805ee29c
0xDosa <findev@rage.trade>
2022-10-22 17:57:48 +0530

    ref: converted all imports to relative imports

--- contracts/interfaces/gmx/IGlpStakingManager.sol
+++ contracts/interfaces/gmx/IGlpStakingManager.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import { IERC4626 } from 'contracts/interfaces/IERC4626.sol';
+import { IERC4626 } from '../IERC4626.sol';
 
 interface IGlpStakingManager is IERC4626 {
     function depositToken(address token, uint256 amount) external returns (uint256 shares);
