17c1364cd1651668d99eae3e91008606cbea2910
protodev-rage <protodev@rage.trade>
2022-10-27 21:36:41 +0530

    ref: remove unused function from SafeCast

--- contracts/libraries/SafeCast.sol
+++ contracts/libraries/SafeCast.sol
@@ -5,15 +5,6 @@ pragma solidity >=0.8.0;
 /// @title Safe casting methods
 /// @notice Contains methods for safely casting between types
 library SafeCast {
-    /// @notice Cast a uint256 to a uint160, revert on overflow
-    /// @param y The uint256 to be downcasted
-    /// @return z The downcasted integer, now type uint160
-    function toUint160(uint256 y) internal pure returns (uint160 z) {
-        unchecked {
-            require((z = uint160(y)) == y, 'Overflow');
-        }
-    }
-
     /// @notice Cast a uint256 to a uint128, revert on overflow
     /// @param y The uint256 to be downcasted
     /// @return z The downcasted integer, now type uint160
@@ -21,25 +12,6 @@ library SafeCast {
         unchecked {
             /* solhint-disable reason-string */
             require((z = uint128(y)) == y);
-        }
-    }
-
-    /// @notice Cast a uint128 to a int128, revert on overflow
-    /// @param y The uint256 to be casted
-    /// @return z The casted integer, now type int256
-    function toInt128(uint128 y) internal pure returns (int128 z) {
-        unchecked {
-            require(y < 2**127, 'Overflow');
-            z = int128(y);
-        }
-    }
-
-    /// @notice Cast a int256 to a int128, revert on overflow or underflow
-    /// @param y The int256 to be downcasted
-    /// @return z The downcasted integer, now type int128
-    function toInt128(int256 y) internal pure returns (int128 z) {
-        unchecked {
-            require((z = int128(y)) == y, 'Overflow');
         }
     }
 
