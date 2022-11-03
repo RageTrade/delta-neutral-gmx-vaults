8eebdeeb098b85dea9b67d25dae61a83f7686624
LeadDev <leaddev@anon.farm>
2022-11-03 14:31:10 +0000

    fix: library events not present in top contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,6 +11,48 @@ import 'hardhat-storage-layout-changes';
 
 import { config } from 'dotenv';
 import { ethers } from 'ethers';
+import { Fragment } from 'ethers/lib/utils';
+import { readJsonSync, writeJsonSync } from 'fs-extra';
+import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names';
+import { task } from 'hardhat/config';
+import nodePath from 'path';
+
+// this compile task override is needed to copy missing abi fragments to respective artifacts (note its not aval to typechain)
+task(TASK_COMPILE, 'Compiles the entire project, building all artifacts').setAction(async (taskArgs, _, runSuper) => {
+  const compileSolOutput = await runSuper(taskArgs);
+
+  copyEventErrorAbis(
+    ['artifacts/contracts/libraries/DnGmxJuniorVaultManager.sol/DnGmxJuniorVaultManager.json'],
+    'artifacts/contracts/vaults/DnGmxJuniorVault.sol/DnGmxJuniorVault.json',
+  );
+
+  function copyEventErrorAbis(froms: string[], to: string) {
+    for (const from of froms) {
+      copyEventErrorAbi(from, to);
+    }
+  }
+
+  function copyEventErrorAbi(from: string, to: string) {
+    const fromArtifact = readJsonSync(nodePath.resolve(__dirname, from));
+    const toArtifact = readJsonSync(nodePath.resolve(__dirname, to));
+    fromArtifact.abi.forEach((fromFragment: Fragment) => {
+      if (
+        // only copy error and event fragments
+        (fromFragment.type === 'error' || fromFragment.type === 'event') &&
+        // if fragment is already in the toArtifact, don't copy it
+        !toArtifact.abi.find(
+          ({ name, type }: Fragment) => name + '-' + type === fromFragment.name + '-' + fromFragment.type,
+        )
+      ) {
+        toArtifact.abi.push(fromFragment);
+      }
+    });
+
+    writeJsonSync(nodePath.resolve(__dirname, to), toArtifact, { spaces: 2 });
+  }
+
+  return compileSolOutput;
+});
 
 config();
 const { ALCHEMY_KEY, LEDGER_ADDRESS } = process.env;
