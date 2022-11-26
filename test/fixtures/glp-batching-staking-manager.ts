71ecdb0f17d08363555316db05403d00c5050a06
0xDosa <90966498+0xDosa@users.noreply.github.com>
2022-11-26 13:22:42 +0530

    Sherlock-Rage8: Batching Manager DOS (#51)
    * fix: added ability for partial batch deposits and depositCap
    
    * test: fixed issues with existing test cases
    
    * test: added additional tests for partial batch deposits
    
    * test: updated storage layout
    
    * chore: add only keeper modifier
    
    Co-authored-by: protodev-rage <protodev@rage.trade>

--- test/fixtures/glp-batching-staking-manager.ts
+++ test/fixtures/glp-batching-staking-manager.ts
@@ -1,10 +1,10 @@
+import { parseUnits } from 'ethers/lib/utils';
 import { deployments } from 'hardhat';
 
 export const glpBatchingStakingManagerFixture = deployments.createFixture(async hre => {
   const gmxBatchingManagerFactory = await hre.ethers.getContractFactory('DnGmxBatchingManager');
 
   const gmxBatchingManager = await gmxBatchingManagerFactory.deploy();
-
   return {
     gmxBatchingManager: gmxBatchingManager,
   };
