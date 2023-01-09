2aa08c6c754385807ba199279cc17b186d1fb67a
0xDosa <90966498+0xDosa@users.noreply.github.com>
2023-01-09 16:24:42 +0530

    Batching Manager Removal From Junior Vault (#61)
    * fix: removed batching manager for staking tokens to glp (as cooldown removed)
    
    * test: updated glp cooldown duration to 0
    
    * fix: removed batching manager from setter, getter and approvals
    
    * test: updated setAdminParams in test cases
    
    * test: updated mocks to fix batching manager test cases

--- contracts/libraries/DnGmxJuniorVaultManager.sol
+++ contracts/libraries/DnGmxJuniorVaultManager.sol
@@ -259,7 +259,7 @@ library DnGmxJuniorVaultManager {
 
                 // deposits weth into batching manager which handles the conversion into glp
                 // can be taken back through batch execution
-                glpReceived = state.batchingManager.depositToken(address(state.weth), dnGmxWethShare, usdgAmount);
+                glpReceived = _stakeGlp(state, address(state.weth), dnGmxWethShare, usdgAmount);
             }
 
             if (_seniorVaultWethRewards > state.wethConversionThreshold) {
@@ -713,9 +713,20 @@ library DnGmxJuniorVaultManager {
         // conversion of token into glp using batching manager
         // batching manager handles the conversion due to the cooldown
         // glp transferred to the vault on batch execution
-        uint256 glpReceived = state.batchingManager.depositToken(address(state.usdc), amount, usdgAmount);
+
+        uint256 glpReceived = _stakeGlp(state, address(state.usdc), amount, usdgAmount);
 
         emit GlpSwapped(glpReceived, amount, false);
+    }
+
+    function _stakeGlp(
+        State storage state,
+        address token,
+        uint256 amount,
+        uint256 minUSDG
+    ) internal returns (uint256 glpStaked) {
+        // will revert if notional output is less than minUSDG
+        glpStaked = state.rewardRouter.mintAndStakeGlp(token, amount, minUSDG, 0);
     }
 
     ///@notice rebalances unhedged glp amount
@@ -1082,11 +1093,7 @@ library DnGmxJuniorVaultManager {
         // part1: glp balance in vault
         // part2: glp balance in batching manager
         // part3: pnl on AAVE (usdc deposit by junior tranche (i.e. dnUsdcDeposited) - current borrow value)
-        return
-            state.fsGlp.balanceOf(address(this)) +
-            state.batchingManager.dnGmxJuniorVaultGlpBalance() +
-            unhedgedGlp -
-            borrowValueGlp;
+        return state.fsGlp.balanceOf(address(this)) + unhedgedGlp - borrowValueGlp;
     }
 
     ///@notice returns if the rebalance is valid basis last rebalance time and rebalanceTimeThreshold
