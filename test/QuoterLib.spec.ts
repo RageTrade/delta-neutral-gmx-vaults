3ee767dcbf5243f1a21205a6b8f0939b3ad68179
LeadDev <leaddev@anon.farm>
2023-01-26 12:20:35 +0000

    fix quoter lib tests

--- test/QuoterLib.spec.ts
+++ test/QuoterLib.spec.ts
@@ -39,7 +39,6 @@ describe('QuoterLib', () => {
       usdc.address,
       weth.address,
       wbtc.address,
-      uniswapQuoter.address,
     ])) as QuoterLibTest;
 
     return { test, uniswapQuoter };
