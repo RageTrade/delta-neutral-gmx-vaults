fb0b94be9d508b157514dd7ac6bdba4b29d9c6fb
LeadDev <leaddev@anon.farm>
2023-02-07 15:50:09 +0000

    add batching manager test

--- test/z_UpgradeImpl2.spec.ts
+++ test/z_UpgradeImpl2.spec.ts
@@ -4,14 +4,18 @@ import { formatEther, parseEther } from 'ethers/lib/utils';
 import hre from 'hardhat';
 
 import { impersonateAccount } from '@nomicfoundation/hardhat-network-helpers';
-import { deltaNeutralGmxVaults, fromQ128, toQ128 } from '@ragetrade/sdk';
+import { deltaNeutralGmxVaults, fromQ128, parseUsdc, toQ128 } from '@ragetrade/sdk';
 import { DepositEvent } from '@ragetrade/sdk/dist/typechain/delta-neutral-gmx-vaults/contracts/ERC4626/ERC4626Upgradeable';
 
-import { TransparentUpgradeableProxy__factory } from '../typechain-types';
+import {
+  DnGmxBatchingManager__factory,
+  DnGmxJuniorVault__factory,
+  TransparentUpgradeableProxy__factory,
+} from '../typechain-types';
 import { dnGmxJuniorVaultFixture } from './fixtures/dn-gmx-junior-vault';
 import { activateMainnetFork } from './utils/mainnet-fork';
 
-describe('Mainnet fork', () => {
+describe('Mainnet fork upgrade impl', () => {
   before(async () => {
     await activateMainnetFork({
       network: 'arbitrum-mainnet',
@@ -21,20 +25,23 @@ describe('Mainnet fork', () => {
 
   it('junior vault', async () => {
     const fixtureDeployments = await dnGmxJuniorVaultFixture();
-    const { users, dnGmxJuniorVault } = fixtureDeployments;
+    const { users } = fixtureDeployments;
 
     const mainnetContracts = deltaNeutralGmxVaults.getContractsSync('arbmain', hre.ethers.provider);
 
     const ownerAddress = await mainnetContracts.proxyAdmin.owner();
-    const keeperAddress = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+    const keeperAddress = (await mainnetContracts.dnGmxJuniorVault.getAdminParams()).keeper;
+    const timelockAddress = await mainnetContracts.dnGmxJuniorVault.owner();
 
     await impersonateAccount(ownerAddress);
     await impersonateAccount(keeperAddress);
+    await impersonateAccount(timelockAddress);
     await impersonateAccount(mainnetContracts.proxyAdmin.address);
     await impersonateAccount(mainnetContracts.proxyAdmin.address);
     const ownerSigner = await hre.ethers.getSigner(ownerAddress);
     const proxyAdminSigner = await hre.ethers.getSigner(mainnetContracts.proxyAdmin.address);
     const keeperSigner = await hre.ethers.getSigner(keeperAddress);
+    const timelockSigner = await hre.ethers.getSigner(timelockAddress);
 
     const oldImpl = await TransparentUpgradeableProxy__factory.connect(
       mainnetContracts.dnGmxJuniorVault.address,
@@ -42,35 +49,48 @@ describe('Mainnet fork', () => {
     ).callStatic.implementation();
     expect(oldImpl).to.equal(mainnetContracts.dnGmxJuniorVaultLogic.address);
 
+    const newDnGmxJuniorVault = await hre.ethers.deployContract('DnGmxJuniorVault', {
+      libraries: {
+        ['contracts/libraries/DnGmxJuniorVaultManager.sol:DnGmxJuniorVaultManager']:
+          fixtureDeployments.dnGmxJuniorVaultManager.address,
+      },
+    });
+
     // perform upgrade
     await mainnetContracts.proxyAdmin
       .connect(ownerSigner)
-      .upgrade(mainnetContracts.dnGmxJuniorVault.address, fixtureDeployments.dnGmxJuniorVault.address);
+      .upgrade(mainnetContracts.dnGmxJuniorVault.address, newDnGmxJuniorVault.address);
 
-    const newImpl = await TransparentUpgradeableProxy__factory.connect(
+    const newImplAddress = await TransparentUpgradeableProxy__factory.connect(
       mainnetContracts.dnGmxJuniorVault.address,
       proxyAdminSigner,
     ).callStatic.implementation();
-    expect(newImpl).to.equal(fixtureDeployments.dnGmxJuniorVault.address);
+    expect(newImplAddress).to.equal(newDnGmxJuniorVault.address);
 
-    // 0.5%
+    // using new interface
+    const dnGmxJuniorVault = DnGmxJuniorVault__factory.connect(
+      mainnetContracts.dnGmxJuniorVault.address,
+      hre.ethers.provider,
+    );
 
-    const totalAssetsBeforeV = await mainnetContracts.dnGmxJuniorVault.totalAssets();
+    // post upgrade admin actions
+    await dnGmxJuniorVault.connect(timelockSigner).setParamsV1(0n, fixtureDeployments.dnGmxTraderHedgeStrategy.address);
+    await dnGmxJuniorVault.connect(timelockSigner).grantAllowances();
+
+    const totalAssetsBeforeV = await dnGmxJuniorVault.totalAssets();
     console.log('totalAssetsBeforeRebalance', formatEther(totalAssetsBeforeV));
 
     // trigger rebalance
     await dnGmxJuniorVault.connect(keeperSigner).rebalance();
 
-    const depositAssets = parseEther('2000');
+    const depositAssets = parseEther('20000'); // there is an error with 2000
     const oldDepositor = '0x04808a3aa9507f2354d3f411f86208ba9fa38093';
 
     // state before deposit
-    const maxWithdrawBefore = await mainnetContracts.dnGmxJuniorVault.maxWithdraw(oldDepositor);
-    const totalAssetsBefore = await mainnetContracts.dnGmxJuniorVault.totalAssets();
+    const maxWithdrawBefore = await dnGmxJuniorVault.maxWithdraw(oldDepositor);
+    const totalAssetsBefore = await dnGmxJuniorVault.totalAssets();
     console.log('totalAssetsBefore', formatEther(totalAssetsBefore));
-    await mainnetContracts.dnGmxJuniorVault
-      .connect(fixtureDeployments.users[0])
-      .approve(mainnetContracts.dnGmxJuniorVault.address, depositAssets);
+    await dnGmxJuniorVault.connect(fixtureDeployments.users[0]).approve(dnGmxJuniorVault.address, depositAssets);
     const sharesPreview = await dnGmxJuniorVault.connect(users[0]).previewDeposit(depositAssets);
 
     // deposit
@@ -79,8 +99,8 @@ describe('Mainnet fork', () => {
 
     await dnGmxJuniorVault.connect(keeperSigner).rebalance();
     // state after deposit
-    const maxWithdrawAfter = await mainnetContracts.dnGmxJuniorVault.maxWithdraw(oldDepositor);
-    const totalAssetsAfter = await mainnetContracts.dnGmxJuniorVault.totalAssets();
+    const maxWithdrawAfter = await dnGmxJuniorVault.maxWithdraw(oldDepositor);
+    const totalAssetsAfter = await dnGmxJuniorVault.totalAssets();
     console.log('totalAssetsAfter', formatEther(totalAssetsAfter));
 
     const depositLog = rc.logs.find(l => l.topics[0] === dnGmxJuniorVault.filters.Deposit().topics![0]);
@@ -90,6 +110,66 @@ describe('Mainnet fork', () => {
     expect(depositEvent.args.shares).to.be.eq(sharesPreview);
     expectEqualWithRelativeError(maxWithdrawBefore, maxWithdrawAfter, 0.05);
     expectEqualWithRelativeError(totalAssetsBefore /*.add(depositAssets)*/, totalAssetsAfter, 0.05);
+  });
+
+  it('batching manager', async () => {
+    const fixtureDeployments = await dnGmxJuniorVaultFixture();
+    const { users } = fixtureDeployments;
+
+    const mainnetContracts = deltaNeutralGmxVaults.getContractsSync('arbmain', hre.ethers.provider);
+
+    const ownerAddress = await mainnetContracts.proxyAdmin.owner();
+    const keeperAddress = await mainnetContracts.dnGmxBatchingManager.keeper();
+    const timelockAddress = await mainnetContracts.dnGmxJuniorVault.owner();
+
+    await impersonateAccount(ownerAddress);
+    await impersonateAccount(keeperAddress);
+    await impersonateAccount(timelockAddress);
+    await impersonateAccount(mainnetContracts.proxyAdmin.address);
+    await impersonateAccount(mainnetContracts.proxyAdmin.address);
+    const ownerSigner = await hre.ethers.getSigner(ownerAddress);
+    const proxyAdminSigner = await hre.ethers.getSigner(mainnetContracts.proxyAdmin.address);
+    const keeperSigner = await hre.ethers.getSigner(keeperAddress);
+    const timelockSigner = await hre.ethers.getSigner(timelockAddress);
+
+    const oldDepositor = '0x04808a3aa9507f2354d3f411f86208ba9fa38093';
+
+    // state before upgrade
+    const unclaimedSharesBefore = await mainnetContracts.dnGmxBatchingManager.unclaimedShares(oldDepositor);
+    const roundIdBefore = await mainnetContracts.dnGmxBatchingManager.currentRound();
+
+    const oldImpl = await TransparentUpgradeableProxy__factory.connect(
+      mainnetContracts.dnGmxBatchingManager.address,
+      proxyAdminSigner,
+    ).callStatic.implementation();
+    expect(oldImpl).to.equal(mainnetContracts.dnGmxBatchingManagerLogic.address);
+
+    const newDnGmxBatchingManager = await hre.ethers.deployContract('DnGmxBatchingManager');
+
+    // perform upgrade
+    await mainnetContracts.dnGmxBatchingManager.connect(keeperSigner).pauseDeposit();
+    await mainnetContracts.proxyAdmin
+      .connect(ownerSigner)
+      .upgrade(mainnetContracts.dnGmxBatchingManager.address, newDnGmxBatchingManager.address);
+    await mainnetContracts.dnGmxBatchingManager.connect(keeperSigner).unpauseDeposit();
+
+    const newImplAddress = await TransparentUpgradeableProxy__factory.connect(
+      mainnetContracts.dnGmxBatchingManager.address,
+      proxyAdminSigner,
+    ).callStatic.implementation();
+    expect(newImplAddress).to.equal(newDnGmxBatchingManager.address);
+
+    // using new interface
+    const dnGmxBatchingManager = DnGmxBatchingManager__factory.connect(
+      mainnetContracts.dnGmxBatchingManager.address,
+      hre.ethers.provider,
+    );
+
+    // state before upgrade
+    const unclaimedSharesAfter = await dnGmxBatchingManager.unclaimedShares(oldDepositor);
+    const roundIdAfter = await dnGmxBatchingManager.currentRound();
+    expect(unclaimedSharesBefore).to.be.eq(unclaimedSharesAfter);
+    expect(roundIdBefore).to.be.eq(roundIdAfter);
   });
 });
 
