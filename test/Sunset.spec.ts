15049691314717e8cc5303a5dc5671a0c729503c
protodev-rage <protodev@rage.trade>
2023-06-22 15:18:03 +0530

    chore: fix script for post upgrade

--- test/Sunset.spec.ts
+++ test/Sunset.spec.ts
@@ -12,15 +12,15 @@ describe('Sunset', () => {
     // real mainnet contract instances
     const {
       dnGmxJuniorVault: _dnGmxJuniorVault,
+      dnGmxSeniorVault,
       proxyAdmin,
       dnGmxTraderHedgeStrategy,
     } = deltaNeutralGmxVaults.getContractsSync('arbmain', hre.ethers.provider);
 
-    const { aUsdc, poolAddressProvider } = await aave.getContracts(hre.ethers.provider);
+    const { aUsdc, poolAddressProvider } = aave.getContractsSync('arbmain', hre.ethers.provider);
     const { fsGLP, weth, wbtc, usdc } = tokens.getContractsSync('arbmain', hre.ethers.provider);
 
     // deploy implementation
-
     const quoterLib = await hre.ethers.deployContract('QuoterLib');
 
     const dnGmxJuniorVaultManager = await hre.ethers.deployContract('DnGmxJuniorVaultManager', {
@@ -73,7 +73,7 @@ describe('Sunset', () => {
       st.partialEthHedgeUsdcAmountThreshold,
     );
 
-    await dnGmxJuniorVault.connect(owner).setParamsV1(parseUnits('200', 6), dnGmxTraderHedgeStrategy.address);
+    await dnGmxJuniorVault.connect(owner).setParamsV1(parseUnits('1', 6), dnGmxTraderHedgeStrategy.address);
 
     const signers = await hre.ethers.getSigners();
     const keeperDNVault = await impersonate(ap.keeper);
@@ -107,53 +107,6 @@ describe('Sunset', () => {
     while (await dnGmxJuniorVault.isValidRebalance()) {
       await dnGmxJuniorVault.connect(keeperDNVault).rebalance();
 
-      if (isChanged) {
-        const glpBal = await fsGLP.balanceOf(dnGmxJuniorVault.address);
-        const [btcBorrow, ethBorrow] = await dnGmxJuniorVault.getCurrentBorrows();
-
-        // const btcAmount = btcBorrow;
-        // console.log('btc external amount:', btcBorrow);
-        // const ethAmount = ethBorrow;
-
-        // const usdcBorrowed = await dnGmxJuniorVault.getUsdcBorrowed()
-        // const available = await aUsdc.balanceOf(dnGmxJuniorVault.address)
-        //
-        // const diff = usdcBorrowed.sub(available);
-        // console.log('diff', diff.toString())
-
-        // await usdc.connect(signers[0]).approve(pool.address, diff);
-        // await wbtc.connect(signers[0]).approve(pool.address, btcAmount);
-        // await weth.connect(signers[0]).approve(pool.address, ethAmount);
-
-        // await generateErc20Balance(wbtc, btcAmount, signers[0].address);
-        // await generateErc20Balance(weth, ethAmount, signers[0].address);
-
-        // await pool.connect(signers[0]).repay(wbtc.address, btcAmount, 2, dnGmxJuniorVault.address);
-        // await pool.connect(signers[0]).repay(weth.address, ethAmount, 2, dnGmxJuniorVault.address);
-
-        // console.log('tokens repaid');
-
-        // await pool.supply(usdc.address, diff, dnGmxJuniorVault.address, 0);
-
-        await dnGmxJuniorVault.connect(owner).repay();
-
-        console.log('usdc repaid');
-
-        const ta = await dnGmxJuniorVault.totalAssets();
-        const ub = await dnGmxJuniorVault.getUsdcBorrowed();
-
-        console.log('iteration:', i);
-        console.log('glp_bal:', formatUnits(glpBal));
-        console.log('token_borrows:', formatUnits(btcBorrow, 8), formatUnits(ethBorrow));
-        console.log('usdc_borrowed:', formatUnits(ub, 6));
-        console.log('total_assets:', formatUnits(ta));
-        console.log('dnUsdcDeposited:', await dnGmxJuniorVault.dnUsdcDeposited());
-        console.log('ausdc bal:', await aUsdc.balanceOf(dnGmxJuniorVault.address));
-        console.log('------------------');
-
-        break;
-      }
-
       await arb(signers[0], wbtc.address, weth.address, 500, true);
       await arb(signers[0], weth.address, usdc.address, 500, true);
 
@@ -167,8 +120,73 @@ describe('Sunset', () => {
       console.log('glp_bal:', formatUnits(glpBal));
       console.log('token_borrows:', formatUnits(btcBorrow, 8), formatUnits(ethBorrow));
       console.log('usdc_borrowed:', formatUnits(ub, 6));
-      console.log('total_assets:', formatUnits(ta));
+      console.log('total_assets_junior:', formatUnits(ta));
+      console.log('total_assets_senior:', formatUnits(await dnGmxSeniorVault.totalAssets(), 6));
+      console.log('aUsdc_bal:', formatUnits(await aUsdc.balanceOf(dnGmxJuniorVault.address), 6));
       console.log('------------------');
+
+      if (isChanged) {
+        const glpBal = await fsGLP.balanceOf(dnGmxJuniorVault.address);
+        const [btcBorrow, ethBorrow] = await dnGmxJuniorVault.getCurrentBorrows();
+
+        const btcAmount = btcBorrow;
+        const ethAmount = ethBorrow;
+        console.log('btc external amount:', btcBorrow);
+        console.log('eth external amount:', ethBorrow);
+
+        const usdcBorrowed = await dnGmxJuniorVault.getUsdcBorrowed();
+        const available = await aUsdc.balanceOf(dnGmxJuniorVault.address);
+        //
+        const diff = usdcBorrowed.sub(available);
+        console.log('diff', diff.toString());
+
+        if (btcAmount.gt(0)) {
+          await wbtc.connect(signers[0]).approve(pool.address, btcAmount);
+          await generateErc20Balance(wbtc, btcAmount, signers[0].address);
+          await pool.connect(signers[0]).repay(wbtc.address, btcAmount, 2, dnGmxJuniorVault.address);
+          console.log('repaid btc externally');
+        }
+
+        if (ethAmount.gt(0)) {
+          await weth.connect(signers[0]).approve(pool.address, ethAmount);
+          await generateErc20Balance(weth, ethAmount, signers[0].address);
+          await pool.connect(signers[0]).repay(weth.address, ethAmount, 2, dnGmxJuniorVault.address);
+          console.log('repaid eth externally');
+        }
+
+        if (diff.gt(0)) {
+          await usdc.connect(signers[0]).approve(pool.address, diff);
+          await pool.supply(usdc.address, diff, dnGmxJuniorVault.address, 0);
+          console.log('diff > 0, supplying usdc externally');
+        }
+        await dnGmxJuniorVault.connect(owner).repay();
+
+        console.log('repaid');
+
+        await dnGmxJuniorVault.connect(owner).setParamsV1(parseUnits('200', 6), dnGmxTraderHedgeStrategy.address);
+        console.log('changing threshold to 200$');
+
+        // to make is valid rebalance false;
+        await dnGmxJuniorVault.connect(keeperDNVault).rebalance();
+        console.log('external rebalance done');
+
+        const ta = await dnGmxJuniorVault.totalAssets();
+        const ub = await dnGmxJuniorVault.getUsdcBorrowed();
+
+        const [newBtcBorrow, newEthBorrow] = await dnGmxJuniorVault.getCurrentBorrows();
+
+        console.log('iteration:', i);
+        console.log('glp_bal:', formatUnits(glpBal));
+        console.log('token_borrows:', formatUnits(newBtcBorrow, 8), formatUnits(newEthBorrow));
+        console.log('usdc_borrowed:', formatUnits(ub, 6));
+        console.log('total_assets_junior:', formatUnits(ta));
+        console.log('total_assets_senior:', formatUnits(await dnGmxSeniorVault.totalAssets(), 6));
+        console.log('dnUsdcDeposited:', formatUnits(await dnGmxJuniorVault.dnUsdcDeposited(), 6));
+        console.log('aUsdc_bal:', formatUnits(await aUsdc.balanceOf(dnGmxJuniorVault.address), 6));
+        console.log('------------------');
+
+        break;
+      }
 
       const btcPrice = await price(wbtc.address, await hre.ethers.provider.getBlockNumber());
       const btcNotional = parseUnits((Number(formatUnits(btcBorrow, 8)) * btcPrice).toFixed(6).toString(), 6);
@@ -202,17 +220,20 @@ describe('Sunset', () => {
         console.log('chainging...');
       }
 
-      // if(i==2) {
-      // await dnGmxJuniorVault.connect(owner).rebalanceProfit();
-      // break;
-      // }
-
       i++;
     }
-    //
-    // const lp = '0x25330e5588DbA5A22444AaA8A237aa4Dd0744F30';
-    //
-    // await dnGmxJuniorVault.connect(await impersonate(lp)).redeem(dnGmxJuniorVault.balanceOf(lp), lp, lp);
+
+    const juniorLp = '0xfb40932271fc9db9dbf048e80697e2da4aa57250';
+    await dnGmxJuniorVault
+      .connect(await impersonate(juniorLp))
+      .redeem(dnGmxJuniorVault.balanceOf(juniorLp), juniorLp, juniorLp);
+    console.log('junior vault lp withdraw');
+
+    const seniorLp = '0x8ce0d9060263b1b5ad3eff2e1e21736b5ef36936';
+    await dnGmxSeniorVault
+      .connect(await impersonate(seniorLp))
+      .redeem(dnGmxSeniorVault.balanceOf(seniorLp), seniorLp, seniorLp);
+    console.log('senior vault lp withdraw');
   });
 });
 
