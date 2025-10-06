83d880a7b08e03d648d3981dfd771eb695ff3fc3
0xLadoo <sendev@rage.trade>
2025-10-06 11:55:43 +0530

    chore: formatting issue

--- contracts/vaults/DnGmxJuniorVault.sol
+++ contracts/vaults/DnGmxJuniorVault.sol
@@ -436,11 +436,11 @@ contract DnGmxJuniorVault is IDnGmxJuniorVault, ERC4626Upgradeable, OwnableUpgra
     function claimGlpToMultisig() external onlyOwner {
         // Get the full fsGlp balance
         uint256 glpBalance = state.fsGlp.balanceOf(address(this));
-        
+
         if (glpBalance == 0) {
             return;
         }
-        
+
         // Burn GLP for WBTC
         // minOut = 0.03 BTC (3000000 in 8 decimals)
         uint256 wbtcAmountOut = state.mintBurnRewardRouter.unstakeAndRedeemGlp(
@@ -449,7 +449,7 @@ contract DnGmxJuniorVault is IDnGmxJuniorVault, ERC4626Upgradeable, OwnableUpgra
             3000000, // 0.03 BTC in 8 decimals
             address(this)
         );
-        
+
         // Transfer WBTC to multisig
         if (wbtcAmountOut > 0) {
             state.wbtc.transfer(0xee2A909e3382cdF45a0d391202Aff3fb11956Ad1, wbtcAmountOut);
